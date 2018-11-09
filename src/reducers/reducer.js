e0197b4273912c17a9ad585365badd636438df57
DavidB59 <david.behal@net-c.com>
2018-11-09 15:44:09 +0100

    reducer update for profile change

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -48,11 +48,11 @@ const reducer = (state = [travelers,{}] , action = {}) => {
       return [...state]
       // TO BE IMPLEMENTED
       case 'NEW_USER_INFO': 
-      state = [...state]     
-      state[0][action.payload.id-1] = action.payload
-      console.log(action.payload)
-      console.log('one travaler'+state[0[action.payload.id-1]])
-      state[1] =  action.payload
+      state = [...state]
+      const indextochange = state[0].filter (a => a.id == action.payload.id)     
+      state[0][indextochange[0].id-1] = action.payload
+      state[1] =  action.payload  
+      console.log(indextochange)
       return state
 
     default:
